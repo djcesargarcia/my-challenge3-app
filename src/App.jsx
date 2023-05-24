c4b70db9074e6b02a160296228f6020cf2b48cce
djcesargarcia <djcesargarcia@gmail.com>
2023-05-24 11:50:04 +0100

    Added changes

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,7 @@ function App() {
               Noteworthy technology acquisitions 2021
             </h5>
             <p className="font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
+              Holaaa
             </p>
           </Card>
         </div>
